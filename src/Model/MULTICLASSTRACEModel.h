95c078b987b2bd0016184039db401a29b055a2ef
slowbull <huozhouyuan@gmail.com>
2017-09-22 17:42:14 -0400

    add decoupled method

---
+++ src/Model/MULTICLASSTRACEModel.h
@@ -0,0 +1,160 @@
+/*
+* Copyright 2017 [Zhouyuan Huo]
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
+#ifndef _MULTICLASSTRACEMODEL_
+#define _MULTICLASSTRACEMODEL_
+
+#include <sstream>
+#include <math.h>
+#include "Model.h"
+#include "../Layer/Layer.h"
+#include "../Tools/Tools.h"
+
+class MULTICLASSTRACEModel : public Model {
+ private:
+  int n_coords;
+  std::vector<double> model;
+  std::vector<int> dims;
+  int postive=0;
+  int negative=0;
+
+  void Initialize() {
+    // 1 layer fully connected network
+    n_coords = FLAGS_d1*FLAGS_d2;
+
+    // init dims.
+	dims.push_back(FLAGS_d1);
+	dims.push_back(FLAGS_d2);
+
+    // Initialize model.
+	InitWeight(model, dims);
+    }
+
+ public:
+  MULTICLASSTRACEModel(int taskid) : Model(taskid) {
+    Initialize();
+  }
+
+  // setup postive and negative numbers.
+  void SetUp(Datapoint *datapoints) override {
+  }
+
+  double ComputeLoss(Datapoint *datapoints, double& accuracy) override {
+    double loss = 0;
+	int size = datapoints->GetSize();
+	mat w_1 = vec_2_mat(model, 0, dims[0], dims[1]);
+	mat o_1(size, dims[1]);
+	mat onehot_y(size, dims[1]);
+
+	one_hot_encoding(datapoints->GetLabelsRows(0, size-1), onehot_y);
+
+	affine_forward(datapoints->GetFeaturesCols(0, size-1).t(), w_1, o_1);
+	loss = logistic_forward(o_1, onehot_y);
+
+	accuracy = metric_acc_logistic(o_1, onehot_y);
+
+	return loss + ComputeRegularization(); 
+  }
+
+  virtual double ComputeRegularization() override {
+	double regloss = 0;
+	// l2 norm
+	for(int i=0; i<model.size(); i++){
+	  regloss += 0.5 * pow(model[i], 2) * FLAGS_l2_lambda;	
+	}
+	// nuclear norm
+	mat w_1 = vec_2_mat(model, 0, dims[0], dims[1]); 
+	mat U, V;
+	vec s;
+	svd(U, s, V, w_1);
+	regloss += accu(s) * FLAGS_trace_lambda;
+
+	return regloss;
+  }
+
+  virtual void ProximalOperator(std::vector<double> &local_model, double gamma) override{
+	// trace norm operator.
+	mat w_1 = vec_2_mat(local_model, 0, dims[0], dims[1]); 
+	mat U, V;
+	vec s;
+	svd(U, s, V, w_1);
+	mat tmp_s(w_1);
+	tmp_s.zeros();
+	for(size_t i=0; i < s.n_elem; i++){
+		if(s[i] - gamma > 0){
+			tmp_s(i, i) = s[i] - gamma;
+		}
+		else{
+			tmp_s(i, i) = 0;
+		}
+	}
+
+	w_1 = U * tmp_s * V.t();
+
+	local_model	= mat_2_vec(w_1);
+  }
+
+  virtual int NumParameters() override {
+	return n_coords;
+  }
+
+  std::vector<double> & ModelData() override {
+	return model;
+  }
+
+  void PrecomputeCoefficients(Datapoint *datapoints, Gradient *g, std::vector<double> &local_model) override {
+	// use layers 
+	int size = datapoints->GetSize();
+	if (g->coeffs.size() != n_coords) g->coeffs.resize(n_coords);
+	mat w_1 = vec_2_mat(local_model, 0, dims[0], dims[1]);
+	mat grad_1(dims[0], dims[1]);
+	mat dx(size, dims[0]);
+	mat o_1(size, dims[1]);
+	mat dldo_1(size, dims[1]);
+	mat onehot_y(size, dims[1]);
+
+	one_hot_encoding(datapoints->GetLabelsRows(0, size-1), onehot_y);
+
+	// forward
+	affine_forward(datapoints->GetFeaturesCols(0, size-1).t(), w_1, o_1);
+
+	// backward
+	logistic_backward(o_1, onehot_y, dldo_1);
+	affine_backward(datapoints->GetFeaturesCols(0, size-1).t(), w_1, dldo_1, dx, grad_1);
+
+	g->coeffs = mat_2_vec(grad_1);
+  }
+
+  // l2 norm.
+  virtual void ComputeL2Gradient(Gradient *g, std::vector<double> &local_model) override {
+	for (int i=0;  i<NumParameters(); i++) {
+	  g->coeffs[i] += FLAGS_l2_lambda * local_model[i];
+	}
+  }	
+
+  virtual void StoreModel(){
+	std::ofstream out_file("model.out");
+	for(int i=0; i< NumParameters(); i++){
+	  out_file << model[i] << " ";
+	}
+	out_file.close();
+  }
+
+  ~MULTICLASSTRACEModel() {
+  }
+};
+
+#endif
---
+++ src/Trainer/DecoupledServerTrainer.h
@@ -0,0 +1,135 @@
+/*
+* Copyright 2017 [Zhouyuan Huo]
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+#ifndef _DECOUPLED_SERVER_TRAINER_
+#define _DECOUPLED_SERVER_TRAINER_ 
+
+#include "../Datapoint/ARMADatapoint.h"
+#include "../Gradient/Gradient.h"
+#include "../Tools/Tools.h"
+
+
+class DecoupledServerTrainer : public Trainer {
+ public:
+
+  DecoupledServerTrainer(Model *model, Datapoint *datapoints) : Trainer(model, datapoints) {}
+  ~DecoupledServerTrainer() {}
+
+  TrainStatistics Train(Model *model, Datapoint *datapoints, Updater *updater) override {
+ 
+    // Keep track of statistics of training.
+    TrainStatistics stats;
+
+	Datapoint *sub_datapoints = new ARMADatapoint();
+
+	MPI_Status status;
+
+	// Train.
+	Timer gradient_timer;
+	printf("Epoch: 	Time(s): Loss:   Evaluation(AUC or Accuracy): \n");
+	for (int epoch = 0; epoch < FLAGS_n_epochs; epoch++) {
+	  srand(epoch);
+
+      // compute loss and print working time.
+	  this->EpochBegin(epoch, gradient_timer, model, datapoints, &stats);
+
+	  gradient_timer.Tick();
+	  updater->EpochBegin();
+      std::vector<int> delay_counter(FLAGS_num_workers, 1);
+	  std::vector<double> worker_gradient(model->NumParameters(), 0);
+	  std::vector<double> message;
+
+	  for(int iter_counter = 0; iter_counter < FLAGS_in_iters; iter_counter++) {
+		int cur_worker_size = 0;
+		std::vector<int> cur_revceived_workers(FLAGS_num_workers, 0);
+
+		// receive information and update.
+		bool flag_receive = true;
+		while(flag_receive) {
+		  MPI_Probe(MPI_ANY_SOURCE, 101, MPI_COMM_WORLD, &status);	
+		  int taskid = status.MPI_SOURCE;
+		  MPI_Recv(&worker_gradient[0], worker_gradient.size(), MPI_DOUBLE, taskid, 101, MPI_COMM_WORLD, &status);
+
+		  gradient->coeffs = worker_gradient;
+		  updater->ApplyGradient(gradient, 1.0);
+
+		  cur_worker_size += 1;
+		  delay_counter[taskid - 1] = 1;
+		  cur_revceived_workers[taskid - 1] = 1;
+
+		  flag_receive = false;
+		  if ( ((cur_worker_size < FLAGS_group_size) || (max_element(delay_counter) > FLAGS_max_delay)) && (iter_counter < FLAGS_in_iters - 1))
+		    flag_receive = true;
+		  if ( (cur_worker_size < FLAGS_num_workers) && (iter_counter == FLAGS_in_iters - 1)) 
+			flag_receive = true;
+		}
+
+		// build message.
+		std::vector<double> & master_model = model->ModelData();
+		message = master_model;
+		if (iter_counter < FLAGS_in_iters - 1) {
+		  message.push_back(0);
+		  message.push_back(0);
+		}
+		else if(epoch < FLAGS_n_epochs - 1) {
+		  message.push_back(1);
+		  message.push_back(0);
+		}
+		else {
+		  message.push_back(1);
+		  message.push_back(1);
+		}
+
+		// send messages to workers.
+		for(int i = 0; i < FLAGS_num_workers; i++) {
+		  if(cur_revceived_workers[i] == 0)
+			delay_counter[i] += 1;
+		  else {
+			MPI_Send(&message[0], message.size(), MPI_DOUBLE, i+1, 102, MPI_COMM_WORLD);
+		  }
+		}
+	  }
+		
+	  updater->EpochFinish();
+	  gradient_timer.Tock();
+	}
+	model->StoreModel();
+	return stats;
+  }
+
+  virtual void EpochBegin (int epoch, Timer &gradient_timer, Model *model, Datapoint *datapoints, TrainStatistics *stats) override {
+	double cur_time;
+    if(stats->times.size()==0) 
+	  cur_time = 0;
+	else
+	  cur_time = gradient_timer.elapsed + stats->times[stats->times.size()-1];
+
+	double worker_eval = 0, master_eval = 0; 
+	double worker_loss = 0, master_loss = 0;
+	int worker_num = 0, master_num = 0;
+	MPI_Reduce(&worker_num, &master_num, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&worker_eval, &master_eval, 1,  MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&worker_loss, &master_loss, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	master_loss /= master_num; 
+	master_eval /= master_num; 
+
+	Trainer::TrackTimeLoss(cur_time, master_loss, stats);
+	if (FLAGS_print_loss_per_epoch && epoch % FLAGS_interval_print == 0) {
+	  Trainer::PrintTimeLoss(cur_time, master_loss, epoch, master_eval);
+	}
+  }
+};
+
+#endif
